c6f477bbd58de0d9ef40a9b3de8d7bc0dd4c9b91
Josef Homann <josefhomann1@hotmail.com>
2020-04-25 17:23:30 +0200

    Touch keys for navigation

--- src/main.cpp
+++ src/main.cpp
@@ -88,12 +88,98 @@ void MDCallback(void *cbData, const char *type, bool isUnicode, const char *stri
   Serial.flush();
 }
 
+#define MAX_USED_TOUCH 3
+
+#define TOUCH_MODE TOUCH_PAD_NUM7
+#define TOUCH_PREV_SONG TOUCH_PAD_NUM8
+#define TOUCH_NEXT_SONG TOUCH_PAD_NUM9
+
+typedef struct
+{
+    touch_pad_t number_TP;
+    uint16_t lastValue;
+} TTaste;
+
+TTaste tasten[MAX_USED_TOUCH];
+touch_pad_t tochNoGpioOrder[MAX_USED_TOUCH] = {TOUCH_MODE, TOUCH_PREV_SONG, TOUCH_NEXT_SONG};
+static void read_Key_task(void *pvParameter)
+{
+    uint16_t touch_filter_value = 0;
+    printf("Touch-Handler running\n");
+    while (1)
+    {
+        // If open the filter mode, please use this API to get the touch pad count.
+        for (uint8_t i = 0; i < MAX_USED_TOUCH; i++)
+        {
+            auto err = touch_pad_read_filtered(tasten[i].number_TP, &touch_filter_value);
+            if (err != ESP_OK)
+            {
+
+                printf("Error: %i- %s", err, esp_err_to_name(err));
+            }
+            //auto dif = tasten[i].lastValue-touch_filter_value;
+
+            // if(tasten[i].no_GPIO==4)
+            // printf("id:%i, old %i, new %i\n",tasten[i].no_GPIO,tasten[i].lastValue,touch_filter_value);
+
+            if (tasten[i].lastValue > 1000 && touch_filter_value < 1000)
+            {
+                printf("id:%i, old %i, new %i\n", tasten[i].number_TP, tasten[i].lastValue, touch_filter_value);
+                switch (tasten[i].number_TP)
+                {
+                case TOUCH_MODE:
+                 runSerialCommand('r');
+                break;
+                case TOUCH_PREV_SONG:
+                runSerialCommand('p');
+                break;
+                case TOUCH_NEXT_SONG:
+                runSerialCommand('n');
+                break;
+                default:
+                break;
+                }
+                
+            }
+            tasten[i].lastValue = touch_filter_value;
+        }
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+    }
+    printf("Touch-Handler stopped\n");
+    vTaskDelete(NULL);
+}
+
+
+void setupTouch()
+{
+    touch_pad_init();
+    touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
+    touch_pad_config(TOUCH_NEXT_SONG, 0);
+    touch_pad_config(TOUCH_PREV_SONG, 0);
+    touch_pad_config(TOUCH_MODE, 0);
+    
+    touch_pad_filter_start(10);
+
+    for (int i = 0; i < MAX_USED_TOUCH; i++)
+    {
+        tasten[i].number_TP = tochNoGpioOrder[i];
+        touch_pad_read_filtered(tasten[i].number_TP, &tasten[i].lastValue);
+    }
+
+    xTaskCreate(&read_Key_task, "read_Key_task", 2048, NULL, 5, NULL);
+}
+
+
+
+
+
 void setup()
 {
   Serial.begin(115200);
   delay(1000);
   navigationSetup(Serial);
   runSerialCommand('c');
+  setupTouch();
 }
 
 void loop()
