e1c23bfeef73e878a2710d29b7114d068f6357b9
Josef Homann <josefhomann1@hotmail.com>
2020-04-26 07:06:06 +0200

    Navigation with touch keys

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Based on ESP8266Audio library (https://github.com/earlephilhower/ESP8266Audio) f
 
 
 Circuit
+
 Micro-SD|ESP32|Headphone|Touch key
 --------|-------|----------|-------
 VSS|5V (3.3V)|  |  
@@ -15,6 +16,8 @@ SCK|GPIO18|  |
 CS|GPIO05|  |  
 |  |GPIO25|Left|  
 |  |GPIO26|Right|  			
+|  |GPIO24|  | (T5) Gain Minus
+|  |GPIO24|  | (T6) Gain Plus
 |  |GPIO24|  | (T7) Toggel Random Play
 |  |GPIO32|  | (T8) Next Song
 |  |GPIO33|  | (T9) Prev. Song
--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,6 @@
 
 #define SPI_SPEED SD_SCK_MHZ(40)
 
-
-
-
-
 void doSoundLoop()
 {
   if (mp3->isRunning())
@@ -22,7 +18,6 @@ void doSoundLoop()
     }
   }
 }
-
 
 void runSerialCommand(char c)
 {
@@ -88,90 +83,93 @@ void MDCallback(void *cbData, const char *type, bool isUnicode, const char *stri
   Serial.flush();
 }
 
-#define MAX_USED_TOUCH 3
-
+#define MAX_USED_TOUCH 5
+#define TOUCH_GAIN_PLUS TOUCH_PAD_NUM5
+#define TOUCH_GAIN_MINUS TOUCH_PAD_NUM6
 #define TOUCH_MODE TOUCH_PAD_NUM7
 #define TOUCH_PREV_SONG TOUCH_PAD_NUM8
 #define TOUCH_NEXT_SONG TOUCH_PAD_NUM9
 
 typedef struct
 {
-    touch_pad_t number_TP;
-    uint16_t lastValue;
+  touch_pad_t number_TP;
+  uint16_t lastValue;
 } TTaste;
 
 TTaste tasten[MAX_USED_TOUCH];
-touch_pad_t tochNoGpioOrder[MAX_USED_TOUCH] = {TOUCH_MODE, TOUCH_PREV_SONG, TOUCH_NEXT_SONG};
+touch_pad_t tochNoGpioOrder[MAX_USED_TOUCH] = {TOUCH_GAIN_MINUS, TOUCH_GAIN_PLUS, TOUCH_MODE, TOUCH_PREV_SONG, TOUCH_NEXT_SONG};
 static void read_Key_task(void *pvParameter)
 {
-    uint16_t touch_filter_value = 0;
-    printf("Touch-Handler running\n");
-    while (1)
+  uint16_t touch_filter_value = 0;
+  printf("Touch-Handler running\n");
+  while (1)
+  {
+    // If open the filter mode, please use this API to get the touch pad count.
+    for (uint8_t i = 0; i < MAX_USED_TOUCH; i++)
     {
-        // If open the filter mode, please use this API to get the touch pad count.
-        for (uint8_t i = 0; i < MAX_USED_TOUCH; i++)
+      auto err = touch_pad_read_filtered(tasten[i].number_TP, &touch_filter_value);
+      if (err != ESP_OK)
+      {
+
+        printf("Error: %i- %s", err, esp_err_to_name(err));
+      }
+      //auto dif = tasten[i].lastValue-touch_filter_value;
+
+      // if(tasten[i].no_GPIO==4)
+      // printf("id:%i, old %i, new %i\n",tasten[i].no_GPIO,tasten[i].lastValue,touch_filter_value);
+
+      if (tasten[i].lastValue > 1000 && touch_filter_value < 1000)
+      {
+        printf("id:%i, old %i, new %i\n", tasten[i].number_TP, tasten[i].lastValue, touch_filter_value);
+        switch (tasten[i].number_TP)
         {
-            auto err = touch_pad_read_filtered(tasten[i].number_TP, &touch_filter_value);
-            if (err != ESP_OK)
-            {
-
-                printf("Error: %i- %s", err, esp_err_to_name(err));
-            }
-            //auto dif = tasten[i].lastValue-touch_filter_value;
-
-            // if(tasten[i].no_GPIO==4)
-            // printf("id:%i, old %i, new %i\n",tasten[i].no_GPIO,tasten[i].lastValue,touch_filter_value);
-
-            if (tasten[i].lastValue > 1000 && touch_filter_value < 1000)
-            {
-                printf("id:%i, old %i, new %i\n", tasten[i].number_TP, tasten[i].lastValue, touch_filter_value);
-                switch (tasten[i].number_TP)
-                {
-                case TOUCH_MODE:
-                 runSerialCommand('r');
-                break;
-                case TOUCH_PREV_SONG:
-                runSerialCommand('p');
-                break;
-                case TOUCH_NEXT_SONG:
-                runSerialCommand('n');
-                break;
-                default:
-                break;
-                }
-                
-            }
-            tasten[i].lastValue = touch_filter_value;
+        case TOUCH_GAIN_MINUS:
+          runSerialCommand('-');
+          break;
+        case TOUCH_GAIN_PLUS:
+          runSerialCommand('+');
+          break;
+        case TOUCH_MODE:
+          runSerialCommand('r');
+          break;
+        case TOUCH_PREV_SONG:
+          runSerialCommand('p');
+          break;
+        case TOUCH_NEXT_SONG:
+          runSerialCommand('n');
+          break;
+        default:
+          break;
         }
-        vTaskDelay(200 / portTICK_PERIOD_MS);
+      }
+      tasten[i].lastValue = touch_filter_value;
     }
-    printf("Touch-Handler stopped\n");
-    vTaskDelete(NULL);
+    vTaskDelay(200 / portTICK_PERIOD_MS);
+  }
+  printf("Touch-Handler stopped\n");
+  vTaskDelete(NULL);
 }
-
 
 void setupTouch()
 {
-    touch_pad_init();
-    touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
-    touch_pad_config(TOUCH_NEXT_SONG, 0);
-    touch_pad_config(TOUCH_PREV_SONG, 0);
-    touch_pad_config(TOUCH_MODE, 0);
-    
-    touch_pad_filter_start(10);
+  touch_pad_init();
+  touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
+  touch_pad_config(TOUCH_NEXT_SONG, 0);
+  touch_pad_config(TOUCH_PREV_SONG, 0);
+  touch_pad_config(TOUCH_MODE, 0);
+  touch_pad_config(TOUCH_GAIN_PLUS, 0);
+  touch_pad_config(TOUCH_GAIN_MINUS, 0);
 
-    for (int i = 0; i < MAX_USED_TOUCH; i++)
-    {
-        tasten[i].number_TP = tochNoGpioOrder[i];
-        touch_pad_read_filtered(tasten[i].number_TP, &tasten[i].lastValue);
-    }
+  touch_pad_filter_start(10);
 
-    xTaskCreate(&read_Key_task, "read_Key_task", 2048, NULL, 5, NULL);
+  for (int i = 0; i < MAX_USED_TOUCH; i++)
+  {
+    tasten[i].number_TP = tochNoGpioOrder[i];
+    touch_pad_read_filtered(tasten[i].number_TP, &tasten[i].lastValue);
+  }
+
+  xTaskCreate(&read_Key_task, "read_Key_task", 2048, NULL, 5, NULL);
 }
-
-
-
-
 
 void setup()
 {
